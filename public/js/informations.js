191b08732d730b2a26de1a61fb0faaa5769b3207
Thiago Dominicheli <42688245+Thi-ZK@users.noreply.github.com>
2018-12-16 02:14:40 -0200

    Update informations.js

--- public/js/informations.js
+++ public/js/informations.js
@@ -1,5 +1,6 @@
 !function(){
 	var contents = document.querySelectorAll(".information-content-div");
+	var content_direct_container = document.querySelector("#information-content-direct-container");
 	var contents_texts = document.querySelectorAll(
 		".flip-card-back h2," +
 		".information-content-title-container h2," +
@@ -31,6 +32,45 @@
 		});
 	}
 
+	var contents_animation = function(target){
+		document.querySelector("#information-content-direct-container").style.position = "relative";
+		if(window.innerWidth > 1525){
+			if((target.getAttribute("content_index") == "2") || (target.getAttribute("content_index") == "5")){
+				content_direct_container.style.left = "-18%";
+			}
+			if((target.getAttribute("content_index") == "3") || (target.getAttribute("content_index") == "6")){
+				content_direct_container.style.left = "-39%";
+			}
+			if((target.getAttribute("content_index") == "1") || (target.getAttribute("content_index") == "4")){
+				content_direct_container.style.left = "3%";
+			}
+		}else if((window.innerWidth > 1022) && (window.innerWidth < 1525)){
+			if((target.getAttribute("content_index") == "1") || (target.getAttribute("content_index") == "3") || (target.getAttribute("content_index") == "5")){
+				content_direct_container.style.left = "-9%";
+			}else{
+				content_direct_container.style.left = "-35%";
+			}
+		}else if((window.innerWidth < 1022) && (window.innerWidth > 820)){
+			content_direct_container.style.left = "-24%";
+		}else if(window.innerWidth < 820){
+			// content_direct_container.style.
+		}
+		setTimeout(function(){
+			var info_content_cont = document.querySelector("#informations-content-container");
+
+			document.querySelector("#content-selected-details-container").style.visibility = "visible";
+			document.querySelector("#content-selected-details-container").style.opacity = "1";
+			window.innerWidth > 1450 ? info_content_cont.style.marginBottom = "14em" : info_content_cont.style.marginBottom = "20em";
+
+			if(window.innerWidth < 970){
+				info_content_cont.style.marginBottom = "24em";
+			}
+			if(window.innerWidth < 820){
+				info_content_cont.style.marginBottom = "7em";
+			}
+		}, 500);
+	}
+
 	for (var i = 0; i < contents.length; i++) {
 		contents[i].addEventListener("click", function(){
 			texts_manager(contents_texts, false, this);
@@ -41,18 +81,7 @@
                     contents[y].style.visibility = "hidden";
 				}
 			}
-			setTimeout(function(){
-				if(window.innerWidth > 1525){ // make conditions to see in what spot the div is
-					document.querySelector("#information-content-direct-container").style.position = "relative";
-					document.querySelector("#information-content-direct-container").style.left = "-15%";
-				}else if(window.innerWidth > 1022){// make conditions to see in what spot the div is
-					document.querySelector("#information-content-direct-container").style.position = "relative";
-					document.querySelector("#information-content-direct-container").style.left = "-15%";
-				}else{// condition not needed here, everyone is aligned
-					document.querySelector("#information-content-direct-container").style.position = "relative";
-					document.querySelector("#information-content-direct-container").style.left = "-15%";
-				}
-			}, 1000);
+			setTimeout(contents_animation.bind(null, this), 1100);
 		});
 	}
 }();
