83e9e10ac0ad39efc5f9a84919dbafa0de6b12b6
Thiago Dominicheli <42688245+Thi-ZK@users.noreply.github.com>
2019-04-15 15:02:18 +0200

    Update footer_and_header.js

--- middlewares/footer_and_header.js
+++ middlewares/footer_and_header.js
@@ -1,5 +1,5 @@
 newsletter_verification = (req, res, next) => {
-	var verf = /^[a-z0-9áéíóúçâêôã]{1,22}((-|_|\.)[0-9a-záéíóúçâêôã]{2,16}){0,3}@([a-z]{2,8})(\.[a-z]{2,8}){1,3}$/.test(req.body.email);
+	var verf = /^[a-zA-Z0-9áéíóúçâêôã]{1,22}((-|_|\.)[0-9a-záéíóúçâêôã]{2,16}){0,3}@([a-z]{2,8})(\.[a-z]{2,8}){1,3}$/.test(req.body.email);
 	verf ? next() : res.send("invalid_email");
 }
 
