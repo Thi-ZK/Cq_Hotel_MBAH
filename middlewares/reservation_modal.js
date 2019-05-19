cb6301de4a80ee09eee27e8d85a0ae39c5d97e1e
tensai1 <thi.yancha@gmail.com>
2019-05-19 21:45:20 +0200

    resevation_modal adjust sizing

--- middlewares/reservation_modal.js
+++ middlewares/reservation_modal.js
@@ -1,7 +1,9 @@
 var reservation_data_validation = (req, res, next) => {
 	var email_regex = /^[a-zA-Z0-9áéíóúçâêôã]{1,22}((-|_|\.)[0-9a-záéíóúçâêôã]{2,16}){0,3}@([a-z]{2,12})(\.[a-z]{2,8}){1,3}$/;
-	var dates_regex = /^(([1-9])|(1[1-2]))\/(([1-9])|([1-2][1-9])|(3[0-1]))\/19$/;
-
+	var dates_regex = /^(([1-9])|(1[0-2]))\/(([1-9])|([1-2][0-9])|(3[0-1]))\/19$/;
+	console.log(req.body.check_out + "        " + dates_regex.test(req.body.check_out));
+	console.log(req.body.check_in + "        " + dates_regex.test(req.body.check_in));
+	console.log(email_regex.test(req.body.email));
 	if((email_regex.test(req.body.email)) && (dates_regex.test(req.body.check_in)) && (dates_regex.test(req.body.check_out))){
 		next();
 	}else{
